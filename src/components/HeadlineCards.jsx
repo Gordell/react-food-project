974ee96652ab554eba95d8755548ac15e7b3b8b1
Daniyal Usmanov <daniyal@MacBook-Pro.local>
2023-09-27 22:09:57 +0300

    first

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function HeadlineCards() {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+         {/* Card */}
+         <div className='rounded-xl relative'>
+             {/* Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-2'>Sun's Out, BOGO</p>
+                <p className='px-2'>Through 8/26</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://burgerkings.ru/image/cache/catalog/photo/436959869-shvedskij-burger-600x600.jpg" alt="/" />
+         </div>
+          {/* Card */}
+          <div className='rounded-xl relative'>
+             {/* Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-2'>Big Stakes at your home</p>
+                <p className='px-2'>Added Daily</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://www.bostonherald.com/wp-content/uploads/2021/09/FOOD-REVERSE-SEAR-STEAK-2-MCT.jpg?w=1024" alt="/" />
+         </div>
+          {/* Card */}
+          <div className='rounded-xl relative'>
+             {/* Overlay */}
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                <p className='font-bold text-2xl px-2 pt-2'>We Deliver Desserts Too</p>
+                <p className='px-2'>Through 8/26</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src="https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVzc2VydHxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="/" />
+         </div>
+    </div>
+  )
+}
+
+export default HeadlineCards
